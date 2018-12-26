2b96515a1a9ed51b87f8abbacd4592594377e7e6
wenhy <haiyang.wen@56qq.com>
2018-12-26 15:08:38 +0800

    String StringBuilder StringBuffer test

--- src/main/java/top/hiccup/jdk/lang/string/StringBuilderAndBufferTest.java
+++ src/main/java/top/hiccup/jdk/lang/string/StringBuilderAndBufferTest.java
@@ -2,11 +2,11 @@ package top.hiccup.jdk.lang.string;
 
 /**
  * StringBuilder与StringBuffer：
- * <p>
+ *
  * 1、StringBuffer是StringBuilder的线程安全版本（修改数据的方法都被加上了synchronized关键字）。
- * <p>
+ *
  * 2、JDK1.8已经对String的“+”做了优化，javac会自动优化为StringBuilder的操作。
- * <p>
+ *
  * 3、JDK1.9对String做出了巨大改动，底层存储的数据结构有char[]改为了byte[]
  *
  * @author wenhy
@@ -16,11 +16,15 @@ public class StringBuilderAndBufferTest {
 
     /**
      * javac StringConcat.java
-     * 再javap -v StringConcat.class
+     * 再javap -v StringConcat.class查看反编译后的结果
      */
     public static void main(String[] args) {
         String strByBuilder = new StringBuilder().append("aa").append("bb").append("cc").append("dd").toString();
+        // 编译器会直接优化成 “aabbccdd”
         String strByConcat = "aa" + "bb" + "cc" + "dd";
-        System.out.println("My String:" + strByConcat);
+        System.out.println(strByBuilder == strByConcat);
+        // 编译器会优化成 “aabbcc”，然后 new StringBuilder("aabbcc").append(new String("dd"));
+        String strByConcat2 = "aa" + "bb" + "cc" + new String("dd");
+        System.out.println("strByConcat2:" + strByConcat2);
     }
 }
