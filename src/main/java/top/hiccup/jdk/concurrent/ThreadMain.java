7f34deaba9e0fbbd7841725edc535b77483f4efe
wenhy <haiyang.wen@56qq.com>
2019-01-07 21:57:40 +0800

    thread test

--- src/main/java/top/hiccup/jdk/concurrent/ThreadMain.java
+++ src/main/java/top/hiccup/jdk/concurrent/ThreadMain.java
@@ -4,29 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by wenhy on 2017/12/23.
+ * 【并发】
+ * Concurrency，即一段时间内多个任务在执行，但不一定是同时在执行，它们可能是交替在运行，也有可能是串行运行的。
+ * <p>
+ * 【并行】
+ * Parallelism，多个任务在同时执行，单核CPU不会有并行操作，因为一个CPU一次只能执行一条指令，并行操作只存在于多核CPU中。
+ *
+ * @author wenhy
+ * @date 2017/12/23
  */
 public class ThreadMain {
 
+    /**
+     * volatile关键字保证线程间的可见性
+     */
     private static volatile List list = new ArrayList();
 
-    public void add(){
-        list.add("bjsxt");
-    }
-    public int size(){
-        return list.size();
-    }
+    // 去掉volatile则不能保证t2被及时通知到
+//    private static List list = new ArrayList();
 
     public static void main(String[] args) {
-
         final ThreadMain list1 = new ThreadMain();
-
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    for(int i = 0; i <10; i++){
-                        list1.add();
+                    for (int i = 0; i < 10; i++) {
+                        list.add("test" + i);
                         System.out.println("当前线程：" + Thread.currentThread().getName() + "添加了一个元素..");
                         Thread.sleep(500);
                     }
@@ -39,9 +43,9 @@ public class ThreadMain {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(true){
-                    if(list1.size() == 5){
-                        System.out.println("当前线程收到通知：" + Thread.currentThread().getName() + " list size = 5 线程停止..");
+                while (true) {
+                    if (list.size() == 5) {
+                        System.out.println("当前线程收到通知：" + Thread.currentThread().getName() + " list size = " + list.size() + " 线程停止..");
                         throw new RuntimeException();
                     }
                 }
@@ -51,5 +55,4 @@ public class ThreadMain {
         t1.start();
         t2.start();
     }
-
 }
