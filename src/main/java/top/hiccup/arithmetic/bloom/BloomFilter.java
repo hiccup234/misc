adae4d53aa8d138b435726c114bb0f7d131b5094
wenhy <haiyang.wen@56qq.com>
2019-01-28 17:37:58 +0800

    bloom filter test

---
+++ src/main/java/top/hiccup/arithmetic/bloom/BloomFilter.java
@@ -0,0 +1,92 @@
+package top.hiccup.arithmetic.bloom;
+
+/**
+ * 布隆过滤器：（利用了hash的思想）
+ * <p>
+ * 见图：
+ * 首先需要初始化一个二进制的数组，长度设为 L（图中为 8），同时初始值全为 0 。
+ * 当写入一个 A1=1000 的数据时，需要进行 H 次 hash 函数的运算（这里为 2 次）；与 HashMap 有点类似，通过算出的 HashCode 与 L 取模后定位到 0、2 处，将该处的值设为 1。
+ * A2=2000 也是同理计算后将 4、7 位置设为 1。
+ * 当有一个 B1=1000 需要判断是否存在时，也是做两次 Hash 运算，定位到 0、2 处，此时他们的值都为 1 ，所以认为 B1=1000 存在于集合中。
+ * 当有一个 B2=3000 时，也是同理。第一次 Hash 定位到 index=4 时，数组中的值为 1，所以再进行第二次 Hash 运算，结果定位到 index=5 的值为 0，所以认为 B2=3000 不存在于集合中。
+ * <p>
+ * 布隆过滤有以下几个特点：
+ * 1、只要返回数据不存在，则肯定不存在。
+ * 2、返回数据存在，但只能是大概率存在（因为存在hash冲突的情况，冲突越小则过滤越准确，可以考虑多次hash）
+ * 3、同时不能清除其中的数据（原因同2）。
+ *
+ * @author wenhy
+ * @date 2019/1/28
+ */
+public class BloomFilter {
+
+    /**
+     * 数组
+     * 优化：可以利用long（64bit）来存储，然后使用位操作以节省空间
+     */
+    private byte[] data;
+    private byte[] data2;
+    /**
+     * 数组大小
+     */
+    private int dataSize;
+
+    public BloomFilter(int dataSize) {
+        this.dataSize = dataSize;
+        data = new byte[dataSize];
+        data2 = new byte[dataSize];
+    }
+
+    /**
+     * 写入数据
+     */
+    public void add(String key) {
+        int first = hash1(key);
+        int second = hash2(key);
+        data[getLawfulIndex(first, dataSize)] = 1;
+        data2[getLawfulIndex(second, dataSize)] = 1;
+    }
+
+    /**
+     * 判断数据是否存在
+     */
+    public boolean check(String key) {
+        int first = hash1(key);
+        int second = hash2(key);
+        int firstIndex = data[getLawfulIndex(first, dataSize)];
+        if (firstIndex == 0) {
+            return false;
+        }
+        int secondIndex = data2[getLawfulIndex(second, dataSize)];
+        if (secondIndex == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private int getLawfulIndex(int orginIndex, int range) {
+        if (orginIndex > range -1) {
+            return getLawfulIndex( orginIndex/2, range);
+        }
+        return orginIndex;
+    }
+
+    /**
+     * hash算法1
+     */
+    private int hash1(String data) {
+        return Math.abs(data.hashCode());
+    }
+
+    /**
+     * hash算法2
+     */
+    private int hash2(String data) {
+        int seek = 37;
+        int hash = data.hashCode() + seek++;
+        hash += (hash << 3);
+        hash -= (hash >> 7);
+        hash ^= (hash << 11);
+        return Math.abs(hash);
+    }
+}
---
+++ src/main/java/top/hiccup/arithmetic/bloom/BloomFilterTest.java
@@ -0,0 +1,49 @@
+package top.hiccup.arithmetic.bloom;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.hash.Funnels;
+
+/**
+ * 布隆过滤器单元测试
+ *
+ * @VM args: -Xms64m -Xmx64m -XX:+PrintGCDetails -XX:+PrintHeapAtGC
+ *
+ * @author wenhy
+ * @date 2019/1/28
+ */
+public class BloomFilterTest {
+
+    @Test
+    public void bloomFilterTest() {
+        long startTime = System.currentTimeMillis();
+        BloomFilter bloomFilters = new BloomFilter(10000000);
+        for (int i = 0; i < 100000000; i++) {
+            bloomFilters.add(i + "");
+        }
+        Assert.assertTrue(bloomFilters.check(1 + ""));
+        Assert.assertTrue(bloomFilters.check(3 + ""));
+        Assert.assertTrue(bloomFilters.check(999999L + ""));
+        Assert.assertFalse(bloomFilters.check(400230340 + "ff"));
+        System.out.println("耗时：" + (System.currentTimeMillis() - startTime));
+    }
+
+    /**
+     * guava包中提供了布隆过滤器的实现
+     */
+    @Test
+    public void guavaTest() {
+        long startTime = System.currentTimeMillis();
+        // 预计存放的数据量  误报率
+        com.google.common.hash.BloomFilter<Integer> filter = com.google.common.hash.BloomFilter.create(Funnels.integerFunnel(), 10000000, 0.01);
+        for (int i = 0; i < 10000000; i++) {
+            filter.put(i);
+        }
+        Assert.assertTrue(filter.mightContain(1));
+        Assert.assertTrue(filter.mightContain(2));
+        Assert.assertTrue(filter.mightContain(3));
+        Assert.assertFalse(filter.mightContain(10000000));
+        System.out.println("耗时：" + (System.currentTimeMillis() - startTime));
+    }
+}
