45f7387bdb6ac3fbea21910d200f94ca33f19fa1
wenhy <haiyang.wen@56qq.com>
2019-02-16 16:27:09 +0800

    static proxy

---
+++ src/main/java/top/hiccup/schema/proxy/StaticProxyTest.java
@@ -0,0 +1,49 @@
+package top.hiccup.schema.proxy;
+
+import org.junit.Test;
+
+/**
+ * 代理一般分为：静态代理 和 动态代理
+ *
+ * 静态代理其实就是通过代码耦合起来
+ *
+ * @author wenhy
+ * @date 2019/2/16
+ */
+public class StaticProxyTest {
+
+    private class User {};
+
+    public interface UserIntf {
+        boolean add(User user);
+    }
+
+    public class UserIntfImpl implements UserIntf {
+        @Override
+        public boolean add(User user) {
+            System.out.println("正在保存用户信息..");
+            return true;
+        }
+    }
+
+    public class StaticProxy implements UserIntf {
+        private UserIntf userIntf;
+        public StaticProxy(UserIntf userIntf) {
+            this.userIntf = userIntf;
+        }
+        @Override
+        public boolean add(User user) {
+            System.out.println("静态代理--保存用户信息前..");
+            boolean result = userIntf.add(user);
+            System.out.println("静态代理--保存用户信息后..");
+            return result;
+        }
+    }
+
+    @Test
+    public void test() {
+        UserIntf userIntf = new UserIntfImpl();
+        StaticProxy proxy = new StaticProxy(userIntf);
+        proxy.add(new User());
+    }
+}
