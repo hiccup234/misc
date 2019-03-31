46cebb133923c4dbbcb9d7053a3ef3a7a94a85d6
wenhy <haiyang.wen@56qq.com>
2019-03-31 23:15:31 +0800

    LinkedList last k node

---
+++ src/main/java/top/hiccup/algorithm/linklist/L1$LastKNokeTest.java
@@ -0,0 +1,115 @@
+package top.hiccup.algorithm.linklist;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+/**
+ * 求单链表倒数第k个节点
+ *
+ * @author wenhy
+ * @date 2019/3/31
+ */
+public class L1$LastKNokeTest {
+
+    /**
+     * 1、最容易想到的方法：先遍历出链表总长度，再遍历k个节点即可
+     *
+     * @param head 链表头节点
+     * @param k
+     * @return
+     */
+    public static Node nodeK1(Node head, int k) {
+        if (head == null || k <= 0) {
+            return null;
+        }
+        // 1、先计算出链表长度
+        int count = 0;
+        for (Node tmp = head; tmp != null; tmp = tmp.getNext()) {
+            count++;
+        }
+        if (count < k) {
+            return null;
+        }
+        Node tmp = head;
+        // 这儿不能 i < count - k + 1，因为后面还要赋值tmp = tmp.getNext();类似+1的动作
+        for (int i = 0; i < count - k; i++) {
+            tmp = tmp.getNext();
+        }
+        return tmp;
+    }
+
+    /**
+     * 2、递归遍历法：方法1的变种：递归遍历至末尾，从末尾开始返回，每返回一次num--，知道num为0则找到目标节点
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    private static int num = 0;
+    public static Node nodeK2(Node head, int k) {
+        num = k;
+        if (head == null || k <= 0) {
+            return null;
+        }
+        Node node = nodeK2(head.getNext(), k);
+        // 如果不为空则证明找到了倒数k的节点，并直接一路返回
+        if (node != null) {
+            return node;
+        } else {
+            num --;
+            if (num == 0) {
+                // 返回倒数第K个节点
+                return head;
+            }
+            return null;
+        }
+    }
+
+    /**
+     * 3、双指针法：指针1先走k步，然后指针2再同指针1一起走，直到指针1到达末尾，需要注意边界问题
+     *   面试官常爱问的
+     * @param head
+     * @param k
+     * @return
+     */
+    public static Node nodeK3(Node head, int k) {
+        if (head == null || k <= 0) {
+            return null;
+        }
+        Node n2 = null;
+        int count = 0;
+        for (Node n1 = head;n1 != null; n1 = n1.getNext()) {
+            count++;
+            if (count == k) {
+                n2 = n1;
+            }
+        }
+        return n2;
+    }
+
+    @Test
+    public void test1() {
+        Random random = new Random(47);
+        for (int i = 0; i < 100; i++) {
+//            Node head = buildLinkList(random.nextInt(500));
+            Node head = Node.buildLinkedList(100);
+            Node.printLinkedList(head);
+            System.out.println(nodeK1(head, 1));
+        }
+    }
+
+    @Test
+    public void test2() {
+        Node head = Node.buildLinkedList(100);
+        Node.printLinkedList(head);
+        System.out.println(nodeK2(head, 100));
+    }
+
+    @Test
+    public void test3() {
+        Node head = Node.buildLinkedList(100);
+        Node.printLinkedList(head);
+        System.out.println(nodeK2(head, 101));
+    }
+}
---
+++ src/main/java/top/hiccup/algorithm/linklist/Node.java
@@ -0,0 +1,52 @@
+package top.hiccup.algorithm.linklist;
+
+import lombok.Data;
+
+/**
+ * 链表节点
+ *
+ * @author wenhy
+ * @date 2019/3/31
+ */
+@Data
+public class Node {
+    private Object val;
+    private Node next;
+
+    @Override
+    public String toString() {
+        return "node:" + String.valueOf(val);
+    }
+
+    public static Node buildLinkedList(int n) {
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            Node node = new Node();
+            node.setVal(i + 1);
+            if (i == 0) {
+                head = node;
+                tail = node;
+                continue;
+            }
+            tail.setNext(node);
+            tail = node;
+        }
+        return head;
+    }
+
+    public static void printLinkedList(Node head) {
+        if (head == null) {
+            System.out.println("none list");
+        }
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (Node p = head; p != null; p = p.getNext()) {
+            sb.append(++count + ":" + p);
+            if (p.getNext() != null) {
+                sb.append(" -> ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
