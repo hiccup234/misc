ffd7689e3bb7eedc3d33365c1e0a7daea6a5a05f
wenhy <haiyang.wen@56qq.com>
2019-04-15 18:52:54 +0800

    mdc test

--- src/main/java/top/hiccup/jdk/concurrent/ThreadLocalTest.java
+++ src/main/java/top/hiccup/jdk/concurrent/ThreadLocalTest.java
@@ -1,5 +1,9 @@
 package top.hiccup.jdk.concurrent;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * 1、ThreadLocal可用于解决线程安全问题，创建线程隔离的变量，是一种无锁解决线程安全问题的方式。
  *   但是本身并不存在线程安全与不安全的说法，因为是线程私有的，不存在多线程访问
@@ -8,4 +12,13 @@ package top.hiccup.jdk.concurrent;
  * @date 2019/3/19
  */
 public class ThreadLocalTest {
+
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        ThreadLocal<List<String>> threadLocal = new ThreadLocal<>();
+        threadLocal.set(list);
+        while (true) {
+            list.add("test"+new Random().nextInt(50000));
+        }
+    }
 }
---
+++ src/main/java/top/hiccup/jdk/log/MDCTest.java
@@ -0,0 +1,10 @@
+package top.hiccup.jdk.log;
+
+/**
+ * 使用slf4j的MDC机制来实现简单的日志链路追踪，基于ThreadLocal
+ *
+ * @author wenhy
+ * @date 2019/4/15
+ */
+public class MDCTest {
+}
