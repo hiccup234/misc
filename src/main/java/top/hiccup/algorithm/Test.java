f50ed465df8e7250077b1dfaadef24ed082021db
wenhy <haiyang.wen@56qq.com>
2019-06-03 14:14:53 +0800

    heap

--- src/main/java/top/hiccup/algorithm/Test.java
+++ src/main/java/top/hiccup/algorithm/Test.java
@@ -113,4 +113,26 @@ public class Test {
         heap(arr);
         System.out.println(Arrays.toString(arr));
     }
+
+
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    class Solution {
+        public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+
+            return false;
+        }
+    }
+
+
+
 }
--- src/main/java/top/hiccup/jdk/container/MapTest.java
+++ src/main/java/top/hiccup/jdk/container/MapTest.java
@@ -7,15 +7,17 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.WeakHashMap;
 
+import org.junit.Test;
+
 /**
  * 1、Hashtable：早期(1.0)Java类库的哈希表实现，本身是同步的（线程安全），不支持null键和null值
- *
+ * <p>
  * 2、HashMap：不是线程安全的，支持null键和null值，存储在第一个槽位
- *
+ * <p>
  * 3、LinkedHashMap：提供遍历顺序符合插入顺序(通过双向链表实现)
- *
+ * <p>
  * 4、TreeMap：基于红黑树的有序Map(由key的自然序或者指定的Comparator决定)
-
+ *
  * @author wenhy
  * @date 2019/1/3
  */
@@ -31,22 +33,38 @@ public class MapTest {
         return (n < 0) ? 1 : n + 1;
     }
 
-    public static void main(String[] args) {
-//        System.out.println(tableSizeFor(1));
-//        System.out.println(tableSizeFor(2));
-//
-//        HashMap<String, Object> hashMap = new HashMap<>();
-//        hashMap.put(null, null);
-//
-//        Hashtable<String, Object> hashtable = new Hashtable<>();
-//        hashtable.put(null, null);
+    @Test
+    public void test() {
+        System.out.println(tableSizeFor(1));
+        System.out.println(tableSizeFor(2));
+    }
 
+    @Test
+    public void test2() {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put(null, null);
+
+//        Hashtable<String, Object> hashtable = new Hashtable<>();
+////        hashtable.put(null, null);
 
         Map<String, String> m = new WeakHashMap<>(3);
 
-        Map treeMap = new TreeMap();
+        Map<String, Integer> treeMap = new TreeMap<>();
         treeMap.put("sdf", 123);
-        String s;
-        Integer i;
+        treeMap.put("abc", 234);
+        treeMap.entrySet().forEach((entry) -> {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        });
+    }
+
+
+    public static void main(String[] args) {
+        // 2G堆内存（-Xms2G -Xmx2G），大概能存2000W左右（23988213）
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>(1024, 1);
+        while (true) {
+            map.put(count, count++);
+            System.out.println(count);
+        }
     }
 }
