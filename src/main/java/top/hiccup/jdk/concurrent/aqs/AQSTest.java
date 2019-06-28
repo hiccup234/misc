80673fbad6566121b4691d37aaadac7cda48ddb2
wenhy <haiyang.wen@56qq.com>
2019-06-28 18:13:02 +0800

    AQS

--- src/main/java/top/hiccup/jdk/concurrent/aqs/AQSTest.java
+++ src/main/java/top/hiccup/jdk/concurrent/aqs/AQSTest.java
@@ -1,7 +1,6 @@
 package top.hiccup.jdk.concurrent.aqs;
 
 /**
- *
  * JUC的基石：AbstractQueuedSynchronizer
  *
  * AQS中的Node.waitStatus有5种状态：
@@ -10,6 +9,10 @@ package top.hiccup.jdk.concurrent.aqs;
  * CONDITION    -2  处于条件队列里，等待条件成立(signal signalall) 条件成立后会置入获取资源的队列里
  * PROPAGATE    -3  共享模式下使用，头节点获取资源时将后面节点设置为此状态，如果头节点获取资源后还有足够的资源，则后面节点会尝试获取，这个状态主要是为了共享状态下队列里足够多的节点同时获取资源
  *              0   初始状态
+ *
+ * 有一个静态内部类Node节点，通过双向链表构成队列，入队出队都是通过CAS替换的，所以多线程并发下不用锁来同步
+ *
+ * 详见AQS类的详细注释
  *
  * @author wenhy
  * @date 2018/8/8
--- src/main/java/top/hiccup/jdk/concurrent/aqs/CASTest.java
+++ src/main/java/top/hiccup/jdk/concurrent/aqs/CASTest.java
@@ -4,11 +4,9 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
- * 【CAS】
- * (Compare And Swap)比较和替换算法，底层原理是基于CPU指令实现的，不同的CPU架构有不同的指令集，本质是一种乐观锁
+ * CAS(Compare And Swap)比较和替换算法，底层原理是基于CPU指令实现的，不同的CPU架构有不同的指令集（X86为CMPXCHG），本质是一种乐观锁
  *
- * 存在问题:
- * 1.ABA问题：如单链表表头（实现栈功能）变化后的ABA问题
+ * ABA问题：如单链表表头（实现栈功能）变化后的ABA问题
  * 解决办法：增加版本号，对应Java中带版本戳的AtomicStampedReference
  *
  * @author wenhy
@@ -29,7 +27,6 @@ public class CASTest {
             new AtomicStampedReference<>(num100, 0);
 
     public static void main(String[] args) {
-
         // 模拟多线程情况
         boolean a1 = atomicLong.compareAndSet(100, 200);
         // 这里再次改回100
