1c08c7a4f5598bde7a6bceed63ba6cd7207f2eca
wenhy <haiyang.wen@56qq.com>
2019-08-13 18:11:35 +0800

    double check singleton

--- src/main/java/top/hiccup/schema/singleton/C_DoubleCheckSingleton.java
+++ src/main/java/top/hiccup/schema/singleton/C_DoubleCheckSingleton.java
@@ -35,13 +35,12 @@ public class C_DoubleCheckSingleton {
             synchronized (C_DoubleCheckSingleton.class) {
                 if (null == instance) {
                     // new C_DoubleCheckSingleton() 不是原子操作
-
                     // memory = allocate();    // 1:分配对象的内存空间
                     // ctorInstance(memory);   // 2:初始化对象
                     // instance = memory;      // 3:设置instance指向刚才分配的内存地址
                     // 伪代码中的2和3之间，可能会被重排序，所以如果执行顺序按1，3，2的话，线程在执行完3后时间片用完切换线程则可能出现未初始化的对象
+                    // 如果用volatile来修饰则会产生写读屏障storeload，禁止2，3的指令重拍，从而防止出现其他线程获取到未初始化完全的对象
                     // TODO 就算时间片用完切换其他线程也进入不了synchronized代码块，因为原来代码块还没执行完成，所以不会释放锁，也不会出现多次实例化
-                    // 如果用volatile来修饰则会产生写读屏障storeload，禁止2，3的指令重拍
                     instance = new C_DoubleCheckSingleton();
                 }
             }
