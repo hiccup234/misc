aa065517b1fa01e6c90d4303e8d529c770bae3c0
hiccup <291746192@qq.com>
2019-12-29 19:07:18 +0800

    LocalityTest

--- src/main/java/top/hiccup/misc/LocalityTest.java
+++ src/main/java/top/hiccup/misc/LocalityTest.java
@@ -5,7 +5,6 @@ package top.hiccup.misc;
  *
  * @author wenhy
  * @date 2019/10/24
- *
  * @VM -Xms1024m -Xmx8192m
  */
 public class LocalityTest {
@@ -20,7 +19,7 @@ public class LocalityTest {
     private static long cacheLine() {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < max; i++) {
-            arr[i] += 1;
+            arr[i] = 1;
         }
         return System.currentTimeMillis() - startTime;
     }
@@ -29,10 +28,10 @@ public class LocalityTest {
         long startTime = System.currentTimeMillis();
         // 这里步长比cacheLine增加了16倍，理论上“访问内存”的次数少了16倍，性能基本应该能提升16倍左右
         // 但是由于x86缓存行一般64Byte，所以对数组遍历时可以有效利用局部性原理，就算步长增加到16倍，也都还是可以直接访问缓存行，而不用去内存加载
-        for (int i = 0; i < max; i += 16) {
+//        for (int i = 0; i < max; i += 16) {
 //      for (int i = 0; i < max; i += 32) {
-//      for (int i = 0; i < max; i += 64) {
-            arr[i] += 1;
+        for (int i = 0; i < max; i += 64) {
+            arr[i] = 1;
         }
         return System.currentTimeMillis() - startTime;
     }
@@ -40,14 +39,18 @@ public class LocalityTest {
     public static void main(String[] args) {
         long cacheTotal = 0;
         for (int i = 0; i < 10; i++) {
-            cacheTotal += cacheLine();
+            long time = cacheLine();
+            System.out.println("测试1耗时：" + time);
+            cacheTotal += time;
         }
-        System.out.println(cacheTotal / 10);
+        System.out.println("测试1平均耗时：" + cacheTotal / 10);
 
         long noCacheTotal = 0;
         for (int i = 0; i < 10; i++) {
-            noCacheTotal += noCacheLine();
+            long time = noCacheLine();
+            System.out.println("测试2耗时：" + time);
+            noCacheTotal += time;
         }
-        System.out.println(noCacheTotal / 10);
+        System.out.println("测试2平均耗时：" + noCacheTotal / 10);
     }
 }
