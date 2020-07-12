657f1951f880f6a7e8151b921a116cf7667acf9a
hiccup <291746192@qq.com>
2020-07-12 11:47:29 +0800

    设计模式

---
+++ src/main/java/top/hiccup/schema/DesignPattern.java
@@ -0,0 +1,16 @@
+package top.hiccup.schema;
+
+/**
+ * 设计模式：
+ *
+ * 1、创建型 = 简单工厂（不是23种设计模式之一） + 工厂方法 + 抽象工厂 + 单例 + 生成器 + 原型
+ *
+ * 2、结构型
+ *
+ * 3、行为型
+ *
+ * @author wenhy
+ * @date 2018/1/2
+ */
+public class DesignPattern {
+}
