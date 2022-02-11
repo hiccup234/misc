35f48a1ececd5df4aca7873be36b2902f0548747
wenhaiyang3 <wenhaiyang3@jd.com>
2022-02-11 15:44:29 +0800

    graph

--- total/src/main/java/top/hiccup/algorithm/graph/Graph.java
+++ total/src/main/java/top/hiccup/algorithm/graph/Graph.java
@@ -129,5 +129,20 @@ public class Graph {
         }
         System.out.print(t + " ");
     }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(6);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 4);
+        graph.bfs(2,4);
+        System.out.println();
+        graph.dfs(2,4);
+    }
 }
 
